3f8a686a74c1ef64f884f794293836cf7eaac29a
l200180205 <48151230+l200180205@users.noreply.github.com>
2020-06-19 20:28:54 +0700

    Add files via upload

---
+++ L200180205_modul7_H/No. 1.py
@@ -0,0 +1,8 @@
+#nomor1
+
+import re
+f = open('Indonesia.txt','r', encoding='latin1')
+p = r'me[\w\.-]+'
+string = re.findall(p, f.read())
+print(string)
+print ("-------------------------------------------------------------")
